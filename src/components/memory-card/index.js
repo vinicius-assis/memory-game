df17c68337a90c28ca10608d53a8061d5bd0fa96
Vinicius <believe.182@hotmail.com>
2019-01-25 13:35:27 +0000

    Atualizacao da aula 026

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,5 +1,5 @@
-const createMemoryCard = (nameClass, src, alt) => `
-  <article class="memory-card ${nameClass}">
+const createMemoryCard = ({ className, src, alt }) => `
+  <article class="memory-card ${className}">
     <img 
       src='img/${src}.png'
       alt='${alt}' 
@@ -7,28 +7,6 @@ const createMemoryCard = (nameClass, src, alt) => `
     />
   </article>
 `;
-
-const cardCollab = ["", "icon-collabcode", "Gueio Mascote da CollabCode"];
-const cardJs = ["-active", "icon-js", "Livro da Linguagem JavaScript"];
-const cardC = ["-active", "icon-c", "Livro da Linguagem C++"];
-const cardHead = ["-active", "icon-cabeca", "Card com a imagem de uma cabeça"];
-const cardCelPhone = [
-  "-active",
-  "icon-celular",
-  "Card com a imagem de um celular"
-];
-const cardJava = ["-active", "icon-java", "Livro da Linguagem JAVA"];
-const cardPhp = ["-active", "icon-php", "Livro da Linguagem PHP"];
-const cardResponsive = [
-  "-active",
-  "icon-responsivo",
-  "Imagem referente a responsividade"
-];
-const cardWoman = [
-  "-active",
-  "icon-woman",
-  "Imagem de uma mulher desenvolvedora"
-];
 
 const $cards = document.querySelectorAll(".memory-card");
 $cards.forEach(item => {
--- src/components/pages/game/index.js
+++ src/components/pages/game/index.js
@@ -1,16 +1,60 @@
 const $root = document.querySelector("#root");
 
+const cardCollab = {
+  src: "icon-collabcode",
+  alt: "Gueio Mascote da CollabCode"
+};
+
+const cardJs = {
+  className: "-active",
+  src: "icon-js",
+  alt: "Livro da Linguagem JavaScript"
+};
+
+const cardC = {
+  className: "-active",
+  src: "icon-c",
+  alt: "Livro da Linguagem C++"
+};
+const cardHead = {
+  className: "-active",
+  src: "icon-cabeca",
+  alt: "Card com a imagem de uma cabeça"
+};
+const cardCelPhone = {
+  className: "-active",
+  src: "icon-celular",
+  alt: "Card com a imagem de um celular"
+};
+const cardJava = {
+  className: "-active",
+  src: "icon-java",
+  alt: "Livro da Linguagem JAVA"
+};
+const cardPhp = {
+  className: "-active",
+  src: "icon-php",
+  alt: "Livro da Linguagem PHP"
+};
+const cardResponsive = {
+  className: "-active",
+  src: "icon-responsivo",
+  alt: "Imagem referente a responsividade"
+};
+const cardWoman = {
+  className: "-active",
+  src: "icon-woman",
+  alt: "Imagem de uma mulher desenvolvedora"
+};
+
 const $cardsWrapper = createCardsWrapper();
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardCollab));
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardC));
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardJs));
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardHead));
-$cardsWrapper.insertAdjacentHTML(
-  "beforeend",
-  createMemoryCard(...cardCelPhone)
-);
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardJava));
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardPhp));
-$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(...cardWoman));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardJs));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardJs));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardJava));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardJava));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardPhp));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardPhp));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardWoman));
+$cardsWrapper.insertAdjacentHTML("beforeend", createMemoryCard(cardWoman));
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
