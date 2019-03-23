062a93514ac0857205d819811535be529c7be03e
Vinicius <believe.182@hotmail.com>
2019-03-23 16:01:32 +0000

    Aula 040 - Desafio Intermediário

--- src/pages/singup/index.js
+++ src/pages/singup/index.js
@@ -3,14 +3,18 @@
 
   const $singupButton = flatButton.render('Singup', true);
   const $loginButton = flatButton.render('Login');
+
+  const $emailForm = logForm.render('email', 'email');
+  const $formWrapper = formWrapper.render($emailForm);
+  
   const $gueioLogo = gueioLogo.render();
   const $titleCollabcode = titleCollabcode.render();
-  
   const $logoWrapper = logoWrapper.render($gueioLogo, $titleCollabcode);
-
+  
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $singupButton);
   $root.insertAdjacentHTML("beforeend", $logoWrapper);
+  $root.insertAdjacentHTML("beforeend", $formWrapper);
 
 })()
