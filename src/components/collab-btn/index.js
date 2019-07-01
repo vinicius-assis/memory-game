6e6f9aec2e743b6c3d1dcebd3662ee97684eb204
Vinicius Assis <believe.182@hotmail.com>
2019-07-01 09:59:47 -0300

    046 - Bug de não dedesenhar a page

--- src/components/collab-btn/index.js
+++ src/components/collab-btn/index.js
@@ -29,7 +29,8 @@ const collabButton = (function() {
 
   module.handleClick = (event, path) => {
     event.preventDefault();
-    window.location.hash = `#/${path}`;
+    location.hash = `#/${path}`;
+    location.reload(true);
   }
 
   module.render = ({content = "", path = ""}) => {
